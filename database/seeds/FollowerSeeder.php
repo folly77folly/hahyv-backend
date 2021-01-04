12e1f04c9cbc0b889e61d9b46fc1320027cf6a73
kingsconsult <kingsconsult001@gmail.com>
2021-01-04 06:02:32 +0100

    home timeline

--- database/seeds/FollowerSeeder.php
+++ database/seeds/FollowerSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
 
 class FollowerSeeder extends Seeder
 {
@@ -11,6 +14,12 @@ class FollowerSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        DB::table('followers')->insert([
+            'user_id' => rand(1, 31),
+            'following_userId' => rand(1, 31),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
