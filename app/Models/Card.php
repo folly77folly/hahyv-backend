758bb68d8375f285ff9ffc494470db53082bc313
kingsconsult <kingsconsult001@gmail.com>
2021-01-08 22:58:28 +0100

    card model

---
+++ app/Models/Card.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\models;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Card extends Model
+{
+
+    protected $fillable = [
+        'user_id',
+        'cardName',
+        'cardNo',
+        'cardExpiringMonth',
+        'cardExpiringYear',
+        'cardCVV'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
