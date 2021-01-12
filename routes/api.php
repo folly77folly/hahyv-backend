48f744314fb3c2eca091534db05aa99a7abc5e82
kingsconsult <kingsconsult001@gmail.com>
2021-01-12 09:27:13 +0100

    stash

--- routes/api.php
+++ routes/api.php
@@ -29,3 +29,66 @@ Route::POST('/password/reset', 'Api\ResetPasswordController@reset')->name('reset
 //Email Verification
 Route::GET('email/resend', 'Api\VerificationController@resend')->name('verification.resend');
 Route::GET('email/verify/{id}/{hash}', 'Api\VerificationController@verify')->name('verification.verify');
+Route::POST('otp/verify', 'Api\VerificationController@verifyOTP')->name('verification.otp');
+
+Route::group(['middleware'=>'auth:api'], function(){
+    //all users
+    Route::GET('/users', 'Api\AuthController@index')->name('allUsers');
+    
+    // User profile
+    Route::GET('profile/preference', 'Api\UserProfileController@preference');
+    Route::PUT('profile/{id}', 'Api\UserProfileController@update')->name('userProfileUpdate');
+    Route::GET('profile/{id}', 'Api\UserProfileController@profile')->name('userProfile');
+    // change password
+    Route::POST('/changepassword', 'Api\AuthController@changePassword');
+    
+    // Get all preferences
+    Route::GET('preferences/', 'Api\PreferenceController@index')->name('preferences');
+    Route::POST('preferences/', 'Api\PreferenceController@store')->name('storePreferences');
+    Route::PUT('preferences/{id}', 'Api\PreferenceController@update')->name('updatePreferences');
+    Route::DELETE('preferences/{id}', 'Api\PreferenceController@destroy')->name('deletePreferences');
+    Route::DELETE('user/{id}', 'Api\UserProfileController@destroy')->name('deleteUser');
+    
+    //follower
+    Route::GET('/following', 'Api\FollowerController@following');
+    Route::GET('/followers', 'Api\FollowerController@followers');
+    Route::resource('follower', Api\FollowerController::class);
+
+    Route::resource('picture', Api\PictureController::class);
+    
+    Route::GET('post/userspost', 'Api\PostController@usersPost');
+    Route::POST('post/likepost', 'Api\PostController@likePost');
+    Route::POST('post/dislikepost', 'Api\PostController@disLikePost');
+    Route::POST('post/postlike', 'Api\PostController@postLike');
+
+    Route::GET('hometimeline', 'Api\HomeTimelineController@index');
+
+    Route::GET('card/usercards', 'Api\CardController@userCards');
+    Route::PUT('card/editcard', 'Api\CardController@editCard');
+    Route::DELETE('card/delete', 'Api\CardController@delete');
+
+    Route::POST('buytoken', 'Api\TokenController@buyToken');
+    
+    
+    Route::apiResource('post', Api\PostController::class);
+
+    Route::apiResource('card', Api\CardController::class);
+
+    //Comments
+    Route::resource('comment', Api\CommentController::class);
+    Route::POST('/comment_unlike', 'Api\CommentLikeController@comment_unlike');
+    Route::resource('commentlike', Api\CommentLikeController::class);
+
+    //Country
+    Route::resource('country', Api\CountryController::class);
+
+    //Bank Details
+    Route::get('/banks', 'Api\BankDetailsController@getCommercialBanks');
+    Route::post('/resolve', 'Api\BankDetailsController@resolveAccountNumber');
+    Route::post('/resolves', 'Api\BankDetailsController@resolveAccount');
+    Route::resource('bank_details', Api\BankDetailsController::class);
+
+    //Bookmark
+    Route::resource('bookmark', Api\BookmarkController::class);
+});
+
