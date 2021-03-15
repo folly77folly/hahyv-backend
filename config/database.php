802c864eeff900c99dbfde454de8d5891efbcafa
folly77folly <folly77folly@gmail.com>
2021-03-15 06:52:37 +0100

    removed my sql connection url

--- config/database.php
+++ config/database.php
@@ -45,7 +45,8 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            // 'url' => env('CLEARDB_DATABASE_URL'),
+            'url' => env('CLEARDB_DATABASE_URL'),
+            // 'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
