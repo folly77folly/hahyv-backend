e4906397ef1736b341374f3ebcc41422c1f7b8e1
folly77folly <folly77folly@gmail.com>
2021-04-26 09:40:51 +0100

    one command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:unSubscribe')->everyMinute();
+        // $schedule->command('command:unSubscribe')->everyMinute();
         $schedule->command('command:subscription_expiry')->everyMinute();
     }
 
